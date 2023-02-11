7bbd11a8ea3c89dfbd8b06497df6dc6ff36962c0
Juan Saa A <juan96saa@gmail.com>
2023-02-11 10:33:42 -0500

    correcion en ruta api

--- public/assets/QSpace.26f0e22f.js
+++
@@ -1,1 +0,0 @@
-import{m as a,k as e}from"./index.7bf797bc.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
---
+++ public/assets/QSpace.7cd7104a.js
@@ -0,0 +1,1 @@
+import{m as a,k as e}from"./index.b3e2412c.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
